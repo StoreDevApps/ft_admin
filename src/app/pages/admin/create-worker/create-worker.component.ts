e48c374d412b7546e5f0a8885e48a677855606da
Jorge Aaron Zambrano Choez <joraazam@espol.edu.ec>
2024-07-30 10:00:08 -0500

    Pantalla de home para admin

--- src/app/pages/admin/create-worker/create-worker.component.ts
+++ src/app/pages/admin/create-worker/create-worker.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-worker',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-worker.component.html',
   styleUrl: './create-worker.component.scss'
 })
 export class CreateWorkerComponent {
+  nombres: string;
+  apellidos: string;
+  correo: string;
+  telefono: string;
+  cedula: string;
 
+  constructor() {
+    this.nombres = '';
+    this.apellidos = '';
+    this.correo = '';
+    this.telefono = '';
+    this.cedula = '';
+  }
+
+  onSubmit() {
+    // Aquí puedes manejar el envío del formulario, por ejemplo, enviando los datos a un servidor
+    console.log('Trabajador creado:', {
+      nombres: this.nombres,
+      apellidos: this.apellidos,
+      correo: this.correo,
+      telefono: this.telefono,
+      cedula: this.cedula
+    });
+    
+    this.nombres = '';
+    this.apellidos = '';
+    this.correo = '';
+    this.telefono = '';
+    this.cedula = '';
+    
+  }
 }
