e89006b0f871f3ed05a44e4e8f51fe1f4c89113c
Lizbeth <lizpevacacela@espol.edu.ec>
2024-08-14 22:45:29 -0500

    Pagina servicios y carrito

--- src/app/pages/public/public-services/public-services.component.ts
+++ src/app/pages/public/public-services/public-services.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { DropdownModule } from 'primeng/dropdown';
+import { AccordionModule } from 'primeng/accordion';
+import { PanelModule } from 'primeng/panel';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-public-services',
   standalone: true,
-  imports: [],
+  imports: [DropdownModule, AccordionModule, PanelModule, CardModule, ButtonModule],
   templateUrl: './public-services.component.html',
   styleUrl: './public-services.component.scss'
 })
 export class PublicServicesComponent {
+  bankOptions = [
+    { label: 'Banco Pichincha', value: 'Banco Pichincha' },
+    { label: 'Banco Guayaquil', value: 'Banco Guayaquil' },
+    { label: 'Banco del Pacífico', value: 'Banco del Pacífico' }
+  ];
 
 }
